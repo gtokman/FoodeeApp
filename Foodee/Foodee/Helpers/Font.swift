48c5ac0a7f95c2a4de27908d7c333c5a6a9d1d70
Gary Tokman <hello@garytokman.me>
2021-09-23 10:25:02 -0400

    fix: add fonts

---
+++ Foodee/Foodee/Helpers/Font.swift
@@ -0,0 +1,14 @@
+//
+//  Font.swift
+//  Foodee
+//
+//  Created by Gary Tokman on 9/23/21.
+//
+
+import Foundation
+
+extension String {
+    static let poppinsRegular = "Poppins-Regular"
+    static let poppinsMedium = "Poppins-Medium"
+    static let poppinsSemibold = "Poppins-SemiBold"
+}
